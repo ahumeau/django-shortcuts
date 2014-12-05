97fa74e65e0ccfadef0cff60007659b315d649a5
ahumeau <humeau.antoine@gmail.com>
2014-12-05 09:31:54 +0000

    Print available aliases before exiting when no argument is supplied

--- django_shortcuts.py
+++ django_shortcuts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -6,39 +7,52 @@ from subprocess import call
 
 ALIASES = {
     # Django
-    'c'  : 'collectstatic --noinput',
+    'c': 'collectstatic --noinput',
     #'r'  : 'runserver',
-    'sd' : 'syncdb',
-    'sp' : 'startproject',
-    'sa' : 'startapp',
-    't'  : 'test',
+    'sd': 'syncdb',
+    'sp': 'startproject',
+    'sa': 'startapp',
+    't': 'test',
 
     # Shell
-    'd'  : 'dbshell',
-    's'  : 'shell',
+    'd': 'dbshell',
+    's': 'shell',
 
     # Auth
     'csu': 'createsuperuser',
     'cpw': 'changepassword',
 
     # South
-    'm'  : 'migrate',
-    'sm' : 'schemamigration',
-    'dm' : 'datamigration',
+    'm': 'migrate',
+    'sm': 'schemamigration',
+    'dm': 'datamigration',
 
     # Haystack
-    'ix' : 'update_index',
+    'ix': 'update_index',
     'rix': 'rebuild_index',
 
     # Django Extensions
-    'sk' : 'generate_secret_key',
+    'sk': 'generate_secret_key',
     'rdb': 'reset_db',
-    'r' : 'runserver_plus',
+    'r': 'runserver_plus',
     'shp': 'shell_plus',
     'url': 'show_urls',
-    'gm' : 'graph_models',
-    'rs' : 'runscript'
+    'gm': 'graph_models',
+    'rs': 'runscript',
+
+    # Django Bower
+    'bower': 'bower_install -- --allow-root',
 }
+
+
+def print_aliases():
+    aliases_list = sorted(ALIASES.items())  # Out of the box sorting works in that case.
+    print 'Available aliases:'
+
+    padding = max([len(alias) for alias, _ in aliases_list])
+
+    for alias in aliases_list:
+        print ('\t\033[32m{: <%d}\033[0m   →   {}' % padding).format(*alias)
 
 
 def run(command=None, *arguments):
@@ -51,6 +65,7 @@ def run(command=None, *arguments):
     """
 
     if command is None:
+        print_aliases()
         sys.exit('django-shortcuts: No argument was supplied, please specify one.')
 
     if command in ALIASES:
